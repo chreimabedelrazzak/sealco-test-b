f8ac3a910f321d3883c52a68c5c08755968e1445
chreimabedelrazzak <chreimabedelrazzak@gmail.com>
2026-01-19 21:10:44 +0200

    fix

--- SimplCommerce/docker-entrypoint.sh
+++ SimplCommerce/docker-entrypoint.sh
@@ -1,12 +1,17 @@
+# #!/bin/bash
+# set -e
+
+# if psql -h simpldb --username postgres -lqt | cut -d \| -f 1 | grep -qw simplcommerce; then
+#     echo "simplcommerce database existed"
+# else
+#     echo "create new database simplcommerce"
+# 	psql -h simpldb --username postgres -c "CREATE DATABASE simplcommerce WITH ENCODING 'UTF8'"
+# 	psql -h simpldb --username postgres -d simplcommerce -a -f /app/dbscript.sql
+# fi
+
+# cd /app && dotnet SimplCommerce.WebHost.dll 
+
 #!/bin/bash
 set -e
-
-if psql -h simpldb --username postgres -lqt | cut -d \| -f 1 | grep -qw simplcommerce; then
-    echo "simplcommerce database existed"
-else
-    echo "create new database simplcommerce"
-	psql -h simpldb --username postgres -c "CREATE DATABASE simplcommerce WITH ENCODING 'UTF8'"
-	psql -h simpldb --username postgres -d simplcommerce -a -f /app/dbscript.sql
-fi
-
-cd /app && dotnet SimplCommerce.WebHost.dll 
+# Just run the app directly using the settings in appsettings.json
+cd /app && dotnet SimplCommerce.WebHost.dll
